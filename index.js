f958afe1c0fd40777b3690191309095547ebfdf7
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-11-24 19:37:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware to serve static files
 app.use(express.static(__dirname + "/public"));
 
+// Get Requests
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/public/about.html");
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`); 
